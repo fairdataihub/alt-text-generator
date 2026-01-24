6ef106ff83df406e67f420020bbcfa41dabde127
Sanjay Soundarajan <sanjay.16@live.com>
2026-01-23 16:21:24 -0800

    style: update favicon

--- web/app/app.vue
+++ web/app/app.vue
@@ -5,7 +5,22 @@ useHead({
   htmlAttrs: {
     lang: "en",
   },
-  link: [{ href: "/favicon.ico", rel: "icon" }],
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-96x96.png",
+      sizes: "96x96",
+    },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    { rel: "shortcut icon", href: "/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
+  ],
   meta: [
     { charset: "utf-8" },
     { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -18,7 +33,12 @@ useHead({
 });
 
 useSeoMeta({
-  titleTemplate: "%s - Alt Text Generator",
+  title: "Alt Text Generator | FAIR Data Innovations Hub",
+  titleTemplate: "Alt Text Generator",
+  description: "Generate alt text for images using AI",
+  ogUrl: "https://alt.fairdataihub.org",
+  twitterCard: "summary_large_image",
+  twitterTitle: "Alt Text Generator",
 });
 </script>
 
